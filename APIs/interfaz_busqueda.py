bd54d8dac36b68278e576c3b03a89479a0539115
Hugo <hcalzado@gmail.com>
2024-01-07 17:33:55 +0100

    Revert "Popout de marcadores"
    This reverts commit 291c858980286dc4785cac8f6d43806ff74ff015.

--- APIs/interfaz_busqueda.py
+++ APIs/interfaz_busqueda.py
@@ -5,7 +5,7 @@ import tkintermapview
 from tkinterweb import HtmlFrame
 import interfaz_carga
 
-def buscar():
+def filtrar():
     def mostrarDatos(marker):
         localidadInput.delete(0, tk.END)
         provinciaInput.delete(0, tk.END)
@@ -19,31 +19,66 @@ def buscar():
                     if centro.get('tipo') == tipo:
                         variableControlTipo.set(tipo)
                         break
-                
-                # Crear una ventana emergente
-                popup = tk.Toplevel()
-                popup.title("Información del Centro")
 
-                # Configurar la posición de la ventana emergente
-                popup.geometry("+{}+{}".format(ventana.winfo_x() + 700, ventana.winfo_y() + 150))
+    url = 'http://127.0.0.1:5004/buscar'
+    tipo = ""
+    if variableControlTipo.get() != "Todos":
+        tipo = variableControlTipo.get()
+    else:
+        tipo = ""
+    # Parámetros a pasar
+    params = {'localidad':localidadInput.get(),'codPostal': codPostalInput.get(), 'provincia':provinciaInput.get(), 'tipo': tipo}
+    response = requests.get(url, params=params)
 
-                # Mostrar más información en la ventana emergente
-                info = f"Nombre: {centro.get('nombre')}\n"
-                info += f"Tipo: {centro.get('tipo')}\n"
-                info += f"Dirección: {centro.get('direccion')}\n"
-                info += f"Código Postal: {centro.get('codigo_postal')}\n"
-                info += f"Teléfono: {centro.get('telefono')}\n"
-                info += f"Descripción: {centro.get('descripcion')}\n"
-                info += f"Localidad: {centro.get('localidad')}\n"
-                info += f"Provincia: {centro.get('provincia')}\n"
-                label = tk.Label(popup, text=info, wraplength=250, justify=tk.LEFT)
-                label.pack()
-               
+    if response.status_code == 200:
+        map_widget.delete_all_marker()
+        resultTable.delete(*resultTable.get_children())
+        centros = response.json()
+        for centro in centros:
+            nombre = centro.get('nombre', 'Nombre no disponible')
+            codPostal = centro.get('codigo_postal', 'Codigo postal no disponible')
+            localidad = centro.get('localidad', 'Localidad no disponible')
+            provincia = centro.get('provincia', 'Provincia no disponible')
+            direccion = centro.get('direccion', 'Dirección no disponible')
+            tipo = centro.get('tipo', 'Tipo no disponible')
+            latitud = centro.get('latitud', 'Latitud no disponible')
+            longitud = centro.get('longitud', 'Longitud no disponible')
+            telefono = centro.get('telefono', 'Teléfono no disponible')
+            descripcion = centro.get('descripcion', 'Descripción no disponible')
+
+            if latitud is not None and longitud is not None:
+                map_widget.set_marker(latitud, longitud, nombre, data=nombre, command=mostrarDatos)
+
+            info_localidad = f"Nombre: {nombre}, Localidad: {localidad}, Provincia: {provincia}, Dirección: {direccion}\n"
+            resultTable.insert('', tk.END, values=(
+            nombre, tipo, direccion, codPostal, telefono, descripcion, localidad, provincia))
+    elif response.status_code == 404:
+        print('Localidad no encontrada')
+        resultTable.delete(*resultTable.get_children())
+        resultTable.insert('', tk.END, values=('no hay resultados'))
+    else:
+        print(f'Error en la solicitud: {response.status_code}')
+
+def cargarMarcadores():
+    def mostrarDatos(marker):
+        localidadInput.delete(0, tk.END)
+        provinciaInput.delete(0, tk.END)
+        codPostalInput.delete(0, tk.END)
+        for centro in centros:
+            if centro.get('nombre') == marker.data:
+                localidadInput.insert(tk.END,centro.get('localidad'))
+                codPostalInput.insert(tk.END, centro.get('codigo_postal'))
+                provinciaInput.insert(tk.END, centro.get('provincia'))
+                for tipo in tipoOptions:
+                    if centro.get('tipo') == tipo:
+                        variableControlTipo.set(tipo)
+                        break
 
     url = 'http://127.0.0.1:5004/getCentros'
     response = requests.get(url)
     if response.status_code == 200:
         centros = response.json()
+        map_widget.delete_all_marker()
         for centro in centros:
             nombre = centro.get('nombre', 'Nombre no disponible')
             codPostal = centro.get('codigo_postal', 'Codigo postal no disponible')
@@ -77,8 +112,7 @@ ventana.resizable(False, False)
 etiqueta = tk.Label(ventana, text="Buscador de centros educativos", font=("Arial", 26))
 etiqueta.place(x=400, y=30)
 
-variableControlTipo = tk.StringVar(ventana)
-variableControlTipo.set("Público")
+
 
 frame_inputs = tk.Frame(ventana)
 frame_inputs.place(x=300, y=120)
@@ -93,13 +127,15 @@ for i, label in enumerate(labels):
 
 localidadInput, codPostalInput, provinciaInput = entries
 
-tipoOptions = ["Público", "Privado", "Concertado", "Otros"]
+tipoOptions = ["Público","Privado", "Concertado", "Otros", "Todos"]
+variableControlTipo = tk.StringVar(ventana)
+variableControlTipo.set(tipoOptions[0])
 tipoInput = tk.OptionMenu(frame_inputs, variableControlTipo, tipoOptions[0], *tipoOptions)
 tipoInput.grid(row=3, column=1, sticky='w')
 
 cancelar = tk.Button(ventana, text="Cancelar", command=ventana.destroy)
 cancelar.place(x=380, y=260)
-aceptar = tk.Button(ventana, text="Aceptar", command=buscar)
+aceptar = tk.Button(ventana, text="Aceptar", command=filtrar)
 aceptar.place(x=480, y=260)
 cargar = tk.Button(ventana, text="Cargar", command=cargar)
 cargar.place(x=380, y=300)
@@ -125,6 +161,7 @@ for col in columns:
 map_widget = tkintermapview.TkinterMapView(ventana, width=400, height=400, corner_radius=0)
 map_widget.place(x=800, y=275, anchor=tk.CENTER)
 map_widget.set_position(40.417278703578596, -3.701168707505883)
-map_widget.set_zoom(10)
+map_widget.set_zoom(6)
+
 
 ventana.mainloop()
